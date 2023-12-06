bc646ca39bb26ec4aafb7dc5d499f5f365d775b5
Endang Ismaya <endang.ismaya@gmail.com>
2023-12-06 14:37:04 +0700

    add session state

---
+++ basics/session_state1.py
@@ -0,0 +1,19 @@
+import streamlit as st
+import streamlit.components.v1 as cmp
+
+text = "State Y"
+if "click" not in st.session_state:
+    st.session_state.click = False
+else:
+    if st.session_state.click is False:
+        text = "State X"
+        st.session_state.click = True
+    else:
+        text = "State Y"
+        st.session_state.click = False
+
+st.button(text)
+
+cmp.iframe(
+    "https://lottie.host/embed/6c269c7a-ebef-49f4-abc8-45f0c1b3b4f5/ffcE5lr9vg.json"
+)
