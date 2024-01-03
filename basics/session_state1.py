f518fe72ba485ba6edb488d5b12c4195b7d36292
Endang Ismaya <endang.ismaya@gmail.com>
2024-01-03 21:15:06 +0700

    add get ip logging

--- basics/session_state1.py
+++ basics/session_state1.py
@@ -17,3 +17,8 @@ st.button(text)
 cmp.iframe(
     "https://lottie.host/embed/6c269c7a-ebef-49f4-abc8-45f0c1b3b4f5/ffcE5lr9vg.json"
 )
+cmp.iframe(
+    "https://lottie.host/embed/5d574a16-6a23-4176-a97b-364233d32193/VYVGfZu1Bn.json"
+)
+
+st.image("data/images/Animation - 1703839876319.gif")
